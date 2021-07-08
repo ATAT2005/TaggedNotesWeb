d7617d9c06afc02c87ec7c3166ab23045af68617
ArtemT <artemt-2005@mail.ru>
2021-07-08 19:52:12 +0500

    ToDo list is updated

--- TaggedNotesWebReact/ClientApp/src/components/FetchData.js
+++ TaggedNotesWebReact/ClientApp/src/components/FetchData.js
@@ -47,14 +47,15 @@ export class FetchData extends Component {
                     <li class="strike">Prevent resizing tag and note column after filtering</li>
                     <li class="strike">Add CSS classes for cells</li>
                     <li class="strike">Reset filters after adding tag or note</li>
-                    <li class="strike">Convert AddNewTag, TagItem and TagList from class components to functional components</li>
+                    <li class="strike">Convert NoteItem, AddNewTag, TagItem and TagList from class components to functional components</li>
                     <li class="strike">Use hooks in Tags, AddNewTag and TagList functional components for working with state and effect</li>
+                    <li class="strike">Allow user to edit tag name/note text inline</li>
+                    <li class="strike">Save tag name/note text changes to DB</li>
+                    <li class="strike">Add checkboxes to tags</li>
+                    <li>Allow user to filter notes by checking tags</li>
                     <li>Make common React component for Notes and Tags</li>
                     <li>Make common method for saving Notes and Tags simultaneously</li>
-                    <li>Allow user to edit note/tag text</li>
                     <li>Allow user to link tags to notes</li>
-                    <li>Allow user to mark tags with checks</li>
-                    <li>Allow user to filter notes by checking tags</li>
                     <li>Add highlight for hovered buttons</li>
                     <li>Add CSS classes for buttons</li>
                     <li>Add more unit tests</li>
