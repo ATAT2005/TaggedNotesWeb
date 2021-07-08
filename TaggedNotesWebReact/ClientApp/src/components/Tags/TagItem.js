7c886178d405402d459caa25bc3659bfa243df73
ArtemT <artemt-2005@mail.ru>
2021-07-08 19:47:53 +0500

    TagItem can be edited inline and checked

--- TaggedNotesWebReact/ClientApp/src/components/Tags/TagItem.js
+++ TaggedNotesWebReact/ClientApp/src/components/Tags/TagItem.js
@@ -1,15 +1,39 @@
-﻿import React from 'react';
+﻿import React, { useState, useRef } from 'react';
 import './../../custom.css';
+import Editable from "../Editable";
 
 /// functional component - a single tag item
- const TagItem = (props) => {
+const TagItem = (props) => {
+    const [tag, setTag] = useState(props.name);
+    const inputRef = useRef();
+
     function onClick() {
         props.removeItem(props.id);
     }
 
+    function updateItem(e) {
+        setTag(e);
+        props.updateItem(props.id, e);
+    }
+
     return (
         <tr>
-            <td className="tagCell">{props.name}</td>
+            <td><input type="checkbox"/></td>
+            <td className="tagCell">
+                <Editable
+                    text={tag}
+                    placeholder="Write a tag name"
+                    childRef={inputRef}
+                    type="input">
+                    <input
+                        ref={inputRef}
+                        type="text"
+                        name="tag"
+                        placeholder="Write a tag name"
+                        value={tag}
+                        onChange={e => updateItem(e.target.value)}/>
+                </Editable>
+            </td>
             <td><button onClick={onClick}>X</button></td>
         </tr>
     );
